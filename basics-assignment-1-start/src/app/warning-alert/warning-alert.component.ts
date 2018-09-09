5388b8d3f1b8e4d018880c462dd7a471f2bb57f2
Tiago Seminotti <tiago.seminotti@gmail.com>
2018-09-09 19:42:35 -0300

    Adding projects

---
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'warning-alert',
+    templateUrl: 'warning-alert.component.html',
+    styles:[`
+        p {
+            color: red
+        }
+    `]
+})
+
+export class WarningAlertComponent{
+
+}
