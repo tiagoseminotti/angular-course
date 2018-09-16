14b87912abc77ceffeb137a610349ef2db8c4b2f
Tiago Seminotti <tiago.seminotti@gmail.com>
2018-09-16 16:19:21 -0300

    Complete angular project

---
+++ ng-complete-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+// export class Ingredient{
+//     public name: string;
+//     public amount: number;
+    
+//     constructor(name: string, amount: number){
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+//Same thing as 
+export class Ingredient{
+    constructor(public name: string, public amount: number){}
+}
